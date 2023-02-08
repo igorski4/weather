40094456113bb36fbb7bb2b3327a4806b3380da9
Igor Karpov <karpov_igorspb@mail.ru>
2023-02-08 13:42:12 +0300

    add min/max temp, feels like

--- scripts/renderWeather.js
+++ scripts/renderWeather.js
@@ -8,13 +8,18 @@ export async function renderWeather(latitude, longitude) {
   wrapper.innerHTML = "";
   const degree = createElement("p", "main__degree");
   const description = createElement("p", "main__weather-description");
+  const degreeStat = createElement("p", "main__degree-stat");
   const changeCity = createElement("input", "main__change-city");
   changeCity.type = "button";
   changeCity.value = "Change city";
   changeCity.addEventListener("click", renderChangeCity);
   const weather = await getWeather(latitude, longitude);
   const city = await getCityByCoordinates(latitude, longitude);
-  degree.innerHTML = `${Math.round(weather.main.temp)}`;
-  description.innerHTML = `Windy in ${city}`;
-  wrapper.prepend(degree, description, changeCity);
+  console.log(weather);
+  degree.innerHTML = `${Math.round(weather.main.temp)}°`;
+  description.innerHTML = `${city}`;
+  degreeStat.innerHTML = `${Math.round(weather.main.temp_max)}°/${Math.round(
+    weather.main.temp_min
+  )}° Feels like ${Math.round(weather.main.feels_like)}°`;
+  wrapper.prepend(degree, description, degreeStat, changeCity);
 }
